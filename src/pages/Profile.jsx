b2645adf935449f9dfd39d77d1655ca0077df55b
Haytam Najim <haytamnajim05@gmail.com>
2025-05-25 12:35:51 +0100

    Améliorer le style et la mise en page des pages de profil et de paramètres : ajuster les couleurs, les tailles, ajouter des ombres et des animations pour une meilleure expérience utilisateur.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,12 +7,6 @@ function Profile() {
     <div className="profile-page">
       <div className="profile-container">
         <div className="profile-header">
-          <Link to="/" className="back-button">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333" width="24px" height="24px">
-              <path d="M0 0h24v24H0z" fill="none"/>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
-            </svg>
-          </Link>
           <div className="avatar-container">
             <div className="user-avatar">H</div>
           </div>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -19,7 +19,7 @@ function Settings() {
         <div className="settings-menu">
           <Link to="/parametres/notifications" className="settings-item">
             <div className="settings-item-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333" width="24px" height="24px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#4a56e2" width="24px" height="24px">
                 <path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"/>
               </svg>
@@ -35,7 +35,7 @@ function Settings() {
           
           <Link to="/parametres/email" className="settings-item">
             <div className="settings-item-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333" width="24px" height="24px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#4a56e2" width="24px" height="24px">
                 <path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
               </svg>
@@ -51,7 +51,7 @@ function Settings() {
           
           <Link to="/parametres/mot-de-passe" className="settings-item">
             <div className="settings-item-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333" width="24px" height="24px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#4a56e2" width="24px" height="24px">
                 <path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
               </svg>
@@ -67,7 +67,7 @@ function Settings() {
           
           <Link to="/parametres/utilisateurs-bloques" className="settings-item">
             <div className="settings-item-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333" width="24px" height="24px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#4a56e2" width="24px" height="24px">
                 <path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm0 0c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
               </svg>
