e0d56566eb63db8a1bbcfa1036c671b4032449ce
haytamnajim <haytamnajim05@gmail.com>
2026-08-29 12:15:14 +0100

    feat: add responsive SimpleFooter component with navigation links and social icons

--- src/components/SimpleFooter.jsx
+++ src/components/SimpleFooter.jsx
@@ -9,19 +9,44 @@ function SimpleFooter() {
   return (
     <footer className="simple-footer">
       <div className="simple-footer-container">
-        {/* Logo simplifié */}
+        {/* Logo */}
         <div className="simple-footer-logo">
           <Link to="/" className="simple-footer-logo-link">
             <span className="simple-footer-logo-text">Matlou7ch</span>
           </Link>
         </div>
 
-        {/* Liens principaux simplifiés */}
-        <div className="simple-footer-links">
-          <Link to="/notre-adn" className="simple-footer-link">Notre ADN</Link>
-          <Link to="/faq" className="simple-footer-link">FAQ</Link>
-          <Link to="/cgu" className="simple-footer-link">Conditions d'utilisation</Link>
-          <Link to="/contact" className="simple-footer-link">Contact</Link>
+        {/* Catégories simplifiées */}
+        <div className="simple-footer-section">
+          <h3 className="simple-footer-title">Catégories</h3>
+          <div className="simple-footer-links">
+            <Link to="/catalogue?category=vetements" className="simple-footer-link">Vêtements</Link>
+            <Link to="/catalogue?category=multimedia" className="simple-footer-link">Multimédia</Link>
+            <Link to="/catalogue?category=maison" className="simple-footer-link">Maison</Link>
+            <Link to="/catalogue?category=sport" className="simple-footer-link">Sport</Link>
+          </div>
+        </div>
+
+        {/* Villes simplifiées */}
+        <div className="simple-footer-section">
+          <h3 className="simple-footer-title">Villes</h3>
+          <div className="simple-footer-links">
+            <Link to="/catalogue?location=Casablanca" className="simple-footer-link">Casablanca</Link>
+            <Link to="/catalogue?location=Rabat" className="simple-footer-link">Rabat</Link>
+            <Link to="/catalogue?location=Marrakech" className="simple-footer-link">Marrakech</Link>
+            <Link to="/catalogue?location=Tanger" className="simple-footer-link">Tanger</Link>
+          </div>
+        </div>
+
+        {/* Liens utiles */}
+        <div className="simple-footer-section">
+          <h3 className="simple-footer-title">Liens</h3>
+          <div className="simple-footer-links">
+            <Link to="/notre-adn" className="simple-footer-link">Notre ADN</Link>
+            <Link to="/faq" className="simple-footer-link">FAQ</Link>
+            <Link to="/cgu" className="simple-footer-link">CGU</Link>
+            <Link to="/contact" className="simple-footer-link">Contact</Link>
+          </div>
         </div>
 
         {/* Réseaux sociaux */}
